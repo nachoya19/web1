1c92ade010b5874ec252d7d6c8c61e9357710aab
nachoya19 <nacho.oya1@gmail.com>
2026-09-24 23:44:18 +0200

    Lógica de interacción, bucle de casillas y eventos de clic para pintar fichas y turnos

--- Misiones/Mision 1/index.js
+++ Misiones/Mision 1/index.js
@@ -7,3 +7,20 @@ const casosGanadores =[[0,1,2],[3,4,5],[6,7,8],[0,3,6],[1,4,7],[2,5,8],[0,4,8],[
 let turno = "❌";
 let juegoActivo = true;
 let tablero = ["","","","","","","","",""];
+
+for (const casilla of casillas) {
+    casilla.addEventListener("click", (event) => {
+    const indice = event.target.dataset.index;
+
+    if(juegoActivo && tablero[indice] === ""){
+        tablero[indice] = turno;
+        event.target.textContent = turno;
+        if(turno === "❌"){
+            turno = "⭕️";
+        }
+        else{
+            turno = "❌";
+        }
+        }
+    });
+}
